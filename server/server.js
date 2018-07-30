85c968a4a60f5cf521fa4cf35771a07017e421dd
HypeDis-2 <30463842+HypeDis@users.noreply.github.com>
2018-07-29 17:35:06 -0700

    changed port variable to work with heroku

--- server/server.js
+++ server/server.js
@@ -7,9 +7,6 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT||3000;
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`listening on port ${port}`);
 })
-//create new express application
-//static middleware
-//create server on port 3000
