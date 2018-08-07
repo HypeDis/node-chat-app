0f00e043381f8cd61c1a58befad1eff0e821bd81
HypeDis-2 <30463842+HypeDis@users.noreply.github.com>
2018-08-07 02:19:45 -0700

    add scrolling function when user is close to the bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom () {
+    // Selectors 
+    let messages = jQuery('#messages');
+    let newMessage = messages.children('li:last-child');
+    //heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 
@@ -18,6 +33,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html); 
+    scrollToBottom();
 });
 
 
@@ -33,7 +49,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
 });
 
 socket.on('disconnect', function () {
